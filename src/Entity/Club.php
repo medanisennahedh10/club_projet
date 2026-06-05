f0bb7669c75223685590c03f21fa82bd438d8dc7
yousseftarhoun <ytarhouni21@gmail.com>
2026-06-05 16:31:48 +0100

    evenement recrutement

--- src/Entity/Club.php
+++ src/Entity/Club.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ClubRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ClubRepository::class)]
@@ -23,7 +25,25 @@ class Club
     private ?string $logo = null;
 
     #[ORM\ManyToOne(inversedBy: 'clubs')]
-    private ?user $proposed_by_id = null;
+    private ?User $proposed_by_id = null;
+
+    /**
+     * @var Collection<int, Evenement>
+     */
+    #[ORM\OneToMany(targetEntity: Evenement::class, mappedBy: 'club_id', orphanRemoval: true)]
+    private Collection $evenements;
+
+    /**
+     * @var Collection<int, Recrutement>
+     */
+    #[ORM\OneToMany(targetEntity: Recrutement::class, mappedBy: 'club_id')]
+    private Collection $recrutements;
+
+    public function __construct()
+    {
+        $this->evenements = new ArrayCollection();
+        $this->recrutements = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -66,14 +86,73 @@ class Club
         return $this;
     }
 
-    public function getProposedById(): ?user
+    public function getProposedById(): ?User
     {
         return $this->proposed_by_id;
     }
 
-    public function setProposedById(?user $proposed_by_id): static
+    public function setProposedById(?User $proposed_by_id): static
     {
         $this->proposed_by_id = $proposed_by_id;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Evenement>
+     */
+    public function getEvenements(): Collection
+    {
+        return $this->evenements;
+    }
+
+    public function addEvenement(Evenement $evenement): static
+    {
+        if (!$this->evenements->contains($evenement)) {
+            $this->evenements->add($evenement);
+            $evenement->setClubId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEvenement(Evenement $evenement): static
+    {
+        if ($this->evenements->removeElement($evenement)) {
+            if ($evenement->getClubId() === $this) {
+                $evenement->setClubId(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Recrutement>
+     */
+    public function getRecrutements(): Collection
+    {
+        return $this->recrutements;
+    }
+
+    public function addRecrutement(Recrutement $recrutement): static
+    {
+        if (!$this->recrutements->contains($recrutement)) {
+            $this->recrutements->add($recrutement);
+            $recrutement->setClubId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRecrutement(Recrutement $recrutement): static
+    {
+        if ($this->recrutements->removeElement($recrutement)) {
+            // set the owning side to null (unless already changed)
+            if ($recrutement->getClubId() === $this) {
+                $recrutement->setClubId(null);
+            }
+        }
 
         return $this;
     }
